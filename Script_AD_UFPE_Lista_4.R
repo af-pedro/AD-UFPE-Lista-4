7c640d83eeaa6dadb3f841a6cb717053e4c0bd82
Pedro Fonseca <49926268+af-pedro@users.noreply.github.com>
2019-04-25 16:43:41 -0300

    Lista 4

--- Script_AD_UFPE_Lista_4.R
+++ Script_AD_UFPE_Lista_4.R
@@ -1,7 +1,15 @@
 #### QUESTAO 1 ####
 #### QUESTAO 2 ####
 
-Atlas_2013 <- read.xls()
+install.packages("readxl")
+library(readxl)
+
+setwd("C:/Users/Pedro/Desktop/Mestrado/Analise de Dados/Material_R/Dados_encontro_2_ufpe/dados_encontro_2_ufpe")
+Atlas_2013 <- read_xlsx("atlas2013_dadosbrutos_pt.xlsx")
+
+head(Atlas_2013)
+print(Atlas_2013)
+
 
 #### QUESTAO 3 ####
 #### QUESTAO 4 ####
