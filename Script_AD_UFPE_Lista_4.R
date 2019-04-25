1d4b350857a848f7d91602c2dacfe3811fef4ba1
Pedro Fonseca <49926268+af-pedro@users.noreply.github.com>
2019-04-25 17:16:22 -0300

    Lista 4

--- Script_AD_UFPE_Lista_4.R
+++ Script_AD_UFPE_Lista_4.R
@@ -3,12 +3,25 @@
 
 install.packages("readxl")
 library(readxl)
+install.packages("dplyr")
+library(dplyr)
 
 setwd("C:/Users/Pedro/Desktop/Mestrado/Analise de Dados/Material_R/Dados_encontro_2_ufpe/dados_encontro_2_ufpe")
 Atlas_2013 <- read_xlsx("atlas2013_dadosbrutos_pt.xlsx")
 
 head(Atlas_2013)
 print(Atlas_2013)
+summary(Atlas_2013)
+dim(Atlas_2013)
+str(docentes_pe)
+
+load("docentes_pe_censo_escolar_2016.RData")
+names(docentes_pe)
+docentes_pe_selecao <- docentes_pe%>% filter(NU_IDADE > 18, NU_IDADE < 70)
+dim(docentes_pe_selecao)
+head(docentes_pe_selecao)
+
+
 
 
 #### QUESTAO 3 ####
