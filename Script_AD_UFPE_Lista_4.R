461fac13b9367df34f27efbf4bb04bd6bdfa4625
Pedro Fonseca <49926268+af-pedro@users.noreply.github.com>
2019-05-02 14:46:06 -0300

    lista 4

--- Script_AD_UFPE_Lista_4.R
+++ Script_AD_UFPE_Lista_4.R
@@ -34,6 +34,7 @@ names(docentes_pe)
 docentes_pe_selecao <- docentes_pe%>% filter(NU_IDADE > 18, NU_IDADE < 70)
 dim(docentes_pe_selecao)
 head(docentes_pe_selecao)
+summary(docentes_pe_selecao$NU_IDADE)
 
 ## 3.
 
@@ -42,8 +43,55 @@ names(matricula_pe)
 matricula_pe_selecao <- matricula_pe%>% filter(NU_IDADE > 1 , NU_IDADE < 25)
 dim(matricula_pe_selecao)
 head(matricula_pe_selecao)
+summary(matricula_pe_selecao$NU_IDADE)
 
 ## 4.
+
+# Matriculas
+matriculas_pe_sel <- matricula_pe %>% group_by(CO_MUNICIPIO) %>%
+  summarise(n_matriculas = n(),
+            alunos_media_idade = mean(NU_IDADE),
+            alunos_fem_sx = sum(TP_SEXO == 2, na.rm = T),
+            alunos_negros = sum(TP_COR_RACA %in% c(2, 3), na.rm = T),
+            alunos_indigenas = sum(TP_COR_RACA == 5, na.rm = T),
+            alunos_cor_nd = sum(TP_COR_RACA == 0, na.rm = T),
+            matriculas_educ_inf = sum(TP_ETAPA_ENSINO %in% c(1, 2), na.rm = T),
+            matriculas_educ_fund = sum(TP_ETAPA_ENSINO %in% c(4:21, 41), na.rm = T),
+            matriculas_educ_medio = sum(TP_ETAPA_ENSINO %in% c(25:38), na.rm = T)
+  )
+
+# verificacao
+dim(matriculas_pe_sel)[1] == length(unique(matricula_pe$CO_MUNICIPIO))
+summary(matriculas_pe_sel)
+
+# Docentes
+docentes_pe_sel <- docentes_pe %>% group_by(CO_MUNICIPIO) %>%
+  summarise(n_docentes = n(),
+            docentes_media_idade = mean(NU_IDADE),
+            docentes_fem_sx = sum(TP_SEXO == 2, na.rm = T),
+            docentes_superior = sum(TP_ESCOLARIDADE == 4, na.rm = T),
+            docentes_contrato = sum(TP_TIPO_CONTRATACAO %in% c(1, 4), na.rm = T)
+  )
+
+# verificacao
+dim(docentes_pe_sel)[1] == length(unique(docentes_pe$CO_MUNICIPIO))
+summary(docentes_pe_sel)
+
+# matriculas
+docentes_matriculas_pe_sel <- docentes_pe_sel %>% full_join(matriculas_pe_sel,
+                                                by = c("CO_MUNICIPIO" = "CO_MUNICIPIO")
+)
+dim(pnud_pe_2010)
+dim(matriculas_pe_sel)
+dim(docentes_matriculas_pe_sel)
+names(docentes_matriculas_pe_sel)
+
+# Média Aritmética / Proporção de docentes 
+docentes_matriculas_pe_sel$n_matriculas/docentes_matriculas_pe_sel$n_docentes
+
+# Grafico
+library(ggplot2)
+ggplot(docentes_matriculas_pe_sel, aes(n_matriculas, n_docentes)) + geom_point()
 
 
 ## 5.
